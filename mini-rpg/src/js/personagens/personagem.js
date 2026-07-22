0291ec7ce7ffc68c9966d9316c8b24437e96130d
Rhennan-ALT <reehsecundario@gmail.com>
2026-07-22 16:08:33 -0300

    feat: cria estrutura inicial da entidade personagem

---
+++ mini-rpg/src/js/personagens/personagem.js
@@ -0,0 +1,10 @@
+const personagem = {  // Futuramente esta entidade poderá ser representada por uma classe.
+    nome: "",
+    vida: 100,
+    nivel: 1,
+
+    // métodos - futuras logicas de ataque e defesa.
+
+    atacar(){},
+    defender(){}
+}
