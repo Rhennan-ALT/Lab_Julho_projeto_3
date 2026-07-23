001f6aa723a48b8a79b29d4659221a4d899fb53b
Rhennan-ALT <reehsecundario@gmail.com>
2026-07-23 12:25:44 -0300

    refactor: alteração nas Espadas, para torna possivel mais tipos

---
+++ mini-rpg/src/js/armas/espadas.js
@@ -0,0 +1,29 @@
+export default espadas
+
+const espadas = [
+    {
+        id: 1,
+        nome: "Espada Simples",
+        dano: 10,
+        range: 1,
+        durabilidade: 50,
+
+        // futuros métodos proprios
+
+        habilidade_arma(){},
+        dropar(){}
+    },
+
+        {
+        id: 2,
+        nome: "Espada Longa",
+        dano: 20,
+        range: 2,
+        durabilidade: 60,
+
+        // futuros métodos proprios
+
+        habilidade_arma(){},
+        dropar(){}
+    }
+]
