7829d65d84354faa49b6f8c26f420632f98b117d
Rhennan-ALT <reehsecundario@gmail.com>
2026-07-23 11:57:48 -0300

    feat: cria estrutura inicial da arma: Arco

---
+++ mini-rpg/src/js/armas/arco.js
@@ -0,0 +1,14 @@
+export default arco
+
+const arco = {
+    nome: Arco,
+    dano: 5,
+    range: 3,
+    durabilidade: 50,
+
+    // futuros métodos proprios
+
+    habilidade_arma(){},
+    dropar(){},
+    recarregar(){}
+}
