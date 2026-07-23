966964593bcd5c87a59b033e02660545d4cfa767
Rhennan-ALT <reehsecundario@gmail.com>
2026-07-23 11:51:50 -0300

    feat: cria estrutura inicial da arma: Espada

---
+++ mini-rpg/src/js/armas/espada.js
@@ -0,0 +1,13 @@
+export default espada
+
+const espada = {
+    nome: Espada,
+    dano: 10,
+    range: 1,
+    durabilidade: 56,
+
+    // futuros métodos proprios
+
+    habilidade_arma(){},
+    dropar(){}
+}
