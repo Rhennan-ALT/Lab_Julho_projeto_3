3c6f51b31b50d8a7ea1cda2e4ab32ca25c7ba3f7
Rhennan-ALT <reehsecundario@gmail.com>
2026-07-23 12:21:44 -0300

    refactor: alteração nos Arcos, para torna possivel mais tipos

---
+++ mini-rpg/src/js/armas/arcos.js
@@ -0,0 +1,33 @@
+export default arcos
+
+const arcos = [
+    
+    {
+    id: 1,
+    nome: "Arco Simples",
+    dano: 5,
+    range: 3,
+    durabilidade: 50,
+
+    // futuros métodos proprios
+
+    habilidade_arma(){},
+    dropar(){},
+    recarregar(){}
+    },
+
+    {
+        id: 2,
+        nome: "Arcos Longo",
+        dano: 10,
+        range: 5,
+        durabilidade: 60,
+
+        // futuros métodos proprios
+
+        habilidade_arma(){},
+        dropar(){},
+        recarregar(){}
+    }
+
+]
