52428dd16c2974c51dd6d81895a46f800099cfdc
Rhennan-ALT <reehsecundario@gmail.com>
2026-07-24 11:25:46 -0300

    feat: cria modelagem inicial das raças

---
+++ mini-rpg/src/js/racas/racas.js
@@ -0,0 +1,30 @@
+const racas = [
+    {
+        id: 5,
+        nome: "Humano",
+        aptidao: "", // ainda preciso pensar na logica pra isso funcionar --- todas as armas
+        bonus: "", // ainda preciso pensar na logica pra isso funcionar --- + 150 de HP base
+
+        habilidade(){} // negociação — consegue negociar itens com mercadores por preços menores 
+    },
+
+    {
+        id: 6,
+        nome: "Elfo",
+        aptidao: "", // ainda preciso pensar na logica pra isso funcionar --- arco +10 de dano
+        bonus: "", // ainda preciso pensar na logica pra isso funcionar --- +50 mana se a classe tiver 
+        
+        habilidade(){} // navegação — sempre conseguem achar seu caminho de volta 
+    },
+
+    {
+        id: 7,
+        nome: "Anão",
+        aptidao: "", // ainda preciso pensar na logica pra isso funcionar --- só pode usar martelo, porém todo dano e X2
+        bonus: "", // ainda preciso pensar na logica pra isso funcionar --- vida(+50) e bônus de defesa(+10) 
+        
+        habilidade(){} // ferreiro — pode aumentar a durabilidade de qualquer arma 
+    }
+]
+
+export default racas 
