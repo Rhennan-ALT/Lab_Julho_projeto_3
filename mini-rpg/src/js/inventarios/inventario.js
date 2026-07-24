41213b59a453057ac30ebb7609e102f885575df1
Rhennan-ALT <reehsecundario@gmail.com>
2026-07-24 10:14:39 -0300

    refactor: modulação melhorada do inventario

--- mini-rpg/src/js/inventarios/inventario.js
+++ mini-rpg/src/js/inventarios/inventario.js
@@ -1,5 +1,14 @@
-export default inventario
+import  espadas  from "/mini-rpg/src/js/armas/espadas.js"
+import arcos from "/mini-rpg/src/js/armas/arcos.js"
 
 const inventario = []
 
+inventario.push(espadas[0])
+inventario.push(arcos[0])
+inventario.push(espadas[1])
+
+console.log(inventario)
+
 // ainda em construção, no agurado do modulo de export e import do roadmap que estou sguindo para finalizar a implementação.
+
+export default inventario
